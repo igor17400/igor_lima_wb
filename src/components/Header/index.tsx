f35871d34f876962d0fadb90a70fd80250f94f01
Igor Lima <igorlima1740@gmail.com>
2021-05-15 11:49:05 -0300

    navbar when accessed via computer is done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,27 +2,36 @@ import styles from './styles.module.scss';
 
 export function Header() {
 	return (
-		<header className={styles.header}>
-			<div className={styles.logoContainer}>
-				<img src='/igor_lima_logo.png' alt='logo' />
+		<nav className={styles.nav}>
+			<div className={styles.logo}>
+				<img src='/igor_lima_logo.png' alt='Igor Lima' />
 			</div>
-			<nav>
-				<ul>
-					<li>
-						<a href='#'>About me</a>
-					</li>
-					<li>
-						<a href='#'>Projects</a>
-					</li>
-					<li>
-						<a href='#'>Contact</a>
-					</li>
-				</ul>
-			</nav>
-			<div className={styles.icons}>
-				<img src='/github.svg' alt='my github' />
-				<img src='/linkedin.svg' alt='my linkedin' />
+			<ul>
+				<li>
+					<a href='#'>About me</a>
+				</li>
+				<li>
+					<a href='#'>Projects</a>
+				</li>
+				<li>
+					<a href='#'>Contact</a>
+				</li>
+				<li className={styles.firstIcon}>
+					<a href='#'>
+						<img src='/github.svg' alt='my github' />
+					</a>
+				</li>
+				<li className={styles.lastIcon}>
+					<a href='#'>
+						<img src='/linkedin.svg' alt='my linkedin' />
+					</a>
+				</li>
+			</ul>
+			<div className={styles.burger}>
+				<div className={styles.line1}></div>
+				<div className={styles.line2}></div>
+				<div className={styles.line3}></div>
 			</div>
-		</header>
+		</nav>
 	);
 }
